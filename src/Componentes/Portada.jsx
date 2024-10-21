de0c19cfffcf05c1417363b97b354f69446b8a4a
GonzaS89 <gnzx89@gmail.com>
2024-10-21 00:53:33 -0300

    second commit

---
+++ src/Componentes/Portada.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import '../Estilos/portada.css'
+
+export const Portada = () => {
+  return (
+    <section className="portada-container">
+        <div className="portada-subcontainer">
+            <span className="bloqueverde"></span>
+            <span className="bloqueazul"></span>
+            <span className="bloqueimg"></span>
+        </div>
+    </section>
+  )
+}
